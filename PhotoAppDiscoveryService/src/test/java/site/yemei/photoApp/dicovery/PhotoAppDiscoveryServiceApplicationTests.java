ad413d64192b8af6ee4e97ae52ae76c52c1c74c3
MeiYe <mabel.yemei@gmail.com>
2020-04-06 15:26:08 -0400

    Add files via upload

---
+++ PhotoAppDiscoveryService/src/test/java/site/yemei/photoApp/dicovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package site.yemei.photoApp.dicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
